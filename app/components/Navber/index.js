94094713141f4f7dda77994bbf791497a2cbb649
PUSELETS <dimamabolo15@gmail.com>
2023-11-11 09:09:31 +0000

    menu

--- app/components/Navber/index.js
+++ app/components/Navber/index.js
@@ -1,10 +1,15 @@
+"use client"
+import { useStateContext } from '@/app/context/StateContext' 
+import { useState } from 'react'
 import { navOption } from '@/app/utils'
 import React from 'react'
+import Link from 'next/link'
+
 
 function NavItem(){
+    const {showNav} = useStateContext();
     return(
-        <div className='navItem'>
-            <ul>
+            <ul className=''>
                 {
                     navOption.map((item)=>(
                         <li className='navOpt' key={item.id}>
@@ -13,20 +18,33 @@ function NavItem(){
                     ))
                 }
             </ul>
-        </div>
     )
 }
 
 export default function Navber() {
+
+    const [menuNav, setMenuNav] = useState(false);
+
   return (
     <div className='navbar-container'>
-         <p className='head'>Food</p>
-         <a className='toggle-bar'>
+         <div className='head'>Food</div>
+         <div 
+            onClick={()=> setMenuNav(!menuNav)}
+            className='toggle-bar'
+
+         >
             <span className='bar'></span>
             <span className='bar'></span>
             <span className='bar'></span>
-         </a>
-         <NavItem /> 
+         </div>
+         <ul className={menuNav? "open" : ""}>
+            <li><Link className='a' href={'/'}>Fruits</Link></li>
+            <li><Link className='a' href={'/'}>Vagetable</Link></li>
+            <li><Link className='a' href={'/'}>Nuddles</Link></li>
+            <li><Link className='a' href={'/'}>Bugger</Link></li>
+            <li><Link className='a' href={'/'}>Beef</Link></li>
+         </ul>
     </div>
+    
   );
 }
--- app/context/StateContext.js
+++ app/context/StateContext.js
@@ -5,9 +5,15 @@ import React, { createContext, useContext, useState, useEffect } from 'react';
 const Context = createContext();
 
 export const StateContext = ({children}) => {
+
+    const [showNav, setShowNav] = useState(false)
+
     return (
         <Context.Provider
-        value={{}}
+        value={{
+            showNav,
+            setShowNav,
+        }}
         >
             {children}
         </Context.Provider>
